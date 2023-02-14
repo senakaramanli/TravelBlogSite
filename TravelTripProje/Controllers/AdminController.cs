fa6cd622eedb6ac206781e3037e05582c0b2162e
Sena <Sena@DESKTOP-4IMMO6J>
2023-02-14 14:23:04 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index(int sayfa=1)
+        {
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).ToList().ToPagedList(sayfa, 5);
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var b = c.Blogs.Find(id);
+            return View("BlogGetir", b);
+        }
+     
+        public ActionResult BlogGuncelle(Blog b)
+        {
+
+           var bl = c.Blogs.Find(b.ID);
+            bl.Aciklama = b.Aciklama;
+            bl.Baslik = b.Baslik;
+            bl.BlogImage = b.BlogImage;
+            bl.Tarih = b.Tarih;
+            c.SaveChanges();
+           return RedirectToAction("Index");
+        }
+
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+
+        public ActionResult YorumGetir(int id)
+        {
+            var y = c.Yorumlars.Find(id);
+            return View("YorumGetir", y);
+        }
+
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+
+            var yorum = c.Yorumlars.Find(y.ID);
+            yorum.Yorum = y.Yorum;
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail;           
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+         Context c = new Context();
+        public ActionResult Index()
+        {
+           
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+
+  
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x=>x.ID==8).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(10).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial4()
+        {
+            var degerler = c.Blogs.Take(3).ToList();
+            return PartialView(degerler);
+        }
+        
+        public PartialViewResult Partial5()
+        {
+            var degerler = c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return PartialView(degerler);
+        }
+    }
+}
